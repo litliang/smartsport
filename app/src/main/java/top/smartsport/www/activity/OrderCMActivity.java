5845122d42474f7d778b3f77334b211a6cb20afb
litliang <litliang@126.com>
2017-09-25 11:23:10 +0800

    ..

--- app/src/main/java/top/smartsport/www/activity/OrderCMActivity.java
+++ app/src/main/java/top/smartsport/www/activity/OrderCMActivity.java
@@ -11,13 +11,14 @@ import org.xutils.view.annotation.ViewInject;
 
 import top.smartsport.www.R;
 import top.smartsport.www.base.BaseActivity;
+import top.smartsport.www.utils.pay.PayUtil;
 
 /**
  * Created by Aaron on 2017/7/28.
  * 比赛--订单确认
  */
 @ContentView(R.layout.activity_order_cm)
-public class OrderCMActivity extends BaseActivity{
+public class OrderCMActivity extends BaseActivity {
 
     @ViewInject(R.id.tv_money)
     private TextView tv_money;
@@ -45,29 +46,36 @@ public class OrderCMActivity extends BaseActivity{
         ivPay[2] = iv_sel_xianxia;
         String type = bundel.getString("type");
         selPayType(Integer.valueOf(type).intValue());
+        id = bundel.getString("product_id");
     }
 
+    String id;
+
     @Event(value = {R.id.rl_zhifubao, R.id.rl_weixin, R.id.rl_xianxia})
-    private void getEvent(View view){
-        switch (view.getId()){
+    private void getEvent(View view) {
+        String action = "";
+        switch (view.getId()) {
             case R.id.rl_zhifubao://支付宝支付
                 type = 1;
                 selPayType(1);
+                action = "aliPay";
                 break;
             case R.id.rl_weixin://微信宝支付
                 type = 2;
                 selPayType(2);
+                action = "wxPay";
                 break;
             case R.id.rl_xianxia://线下宝支付
                 type = 3;
                 selPayType(3);
                 break;
         }
+        PayUtil.pay(this, action, "2", tv_money.getText().toString(), id);
     }
 
     private void selPayType(int index) {
-        for(int i=0; i<3; i++) {
-            if(index == (i+1)) {
+        for (int i = 0; i < 3; i++) {
+            if (index == (i + 1)) {
                 ivPay[i].setImageResource(R.mipmap.check_down);
             } else {
                 ivPay[i].setImageResource(R.mipmap.check_up);
