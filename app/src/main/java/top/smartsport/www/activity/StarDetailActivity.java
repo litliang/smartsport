887c823145a6aa0443fe8dbfd9869dd3eb8a1e69
anyabo <773448548@qq.com>
2017-09-28 16:27:39 +0800

    bug fix :球星详情分享功能

--- app/src/main/java/top/smartsport/www/activity/StarDetailActivity.java
+++ app/src/main/java/top/smartsport/www/activity/StarDetailActivity.java
@@ -1,5 +1,8 @@
 package top.smartsport.www.activity;
 
+import android.text.TextUtils;
+import android.util.Log;
+
 import org.xutils.view.annotation.ContentView;
 
 import java.util.List;
@@ -23,10 +26,15 @@ public class StarDetailActivity extends BaseActivity {
         BaseActivity.callHttp(MapBuilder.build().add("action", "getRecommendPlayers").add("id",getIntent().getStringExtra("id")).get(), this, new FunCallback() {
             @Override
             public void onSuccess(Object result, List object) {
+
+
                 MapConf.with(getBaseContext()).pair("players[0]-name->tv_name").pair("players[0]-team_name->tv_team").pair("players[0]-cover_url->iv_top_pic").pair("players[0]-introduce->tv_introduce_star").pair("players[0]-stage->tv_week").source(((NetEntity)result).getData().toString(),StarDetailActivity.this).toView();
-                setSharetitle(getTextString(R.id.tv_name));
-                setSharetxt(getTextString(R.id.tv_introduce_star));
-                setShareurl(((ImageView)getView(R.id.iv_top_pic)).getUrl());
+                String title = !TextUtils.isEmpty(getTextString(R.id.tv_name)) ? getTextString(R.id.tv_name) : "球星名称";
+                String shareText = !TextUtils.isEmpty(getTextString(R.id.tv_introduce_star)) ? getTextString(R.id.tv_introduce_star) : "球星介绍。图文介绍。";
+                String shareUrl = !TextUtils.isEmpty(((ImageView)getView(R.id.iv_top_pic)).getUrl()) ? ((ImageView)getView(R.id.iv_top_pic)).getUrl() : "--";
+                setSharetitle(title);
+                setSharetxt(shareText);
+                setShareurl(shareUrl);
             }
             @Override
             public void onFailure(Object result, List object) {
